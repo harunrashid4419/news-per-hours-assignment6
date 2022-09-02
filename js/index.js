ea5ba828f110f16b455056fd181d14a17f4e79a0
harunorrashid4419 <harunrashid4419@gmail.com>
2022-09-02 23:41:49 +0600

    news-add

--- js/index.js
+++ js/index.js
@@ -20,3 +20,44 @@ const displayCategory = categorys =>{
 
 loadCategory();
 
+const loadNews = () =>{
+    fetch(`https://openapi.programming-hero.com/api/news/category/01`)
+        .then(res => res.json())
+        .then(data => displayNews(data.data))
+};
+
+const displayNews = allNews =>{
+    const newsCategory = document.getElementById('news-category');
+    allNews.forEach(news =>{
+        console.log(news);
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('d-flex');
+        newsDiv.innerHTML = `
+            <div>
+                <img class="rounded mb-4" src = "${news.thumbnail_url}">
+            </div>
+            <div class = "ps-5">
+                <h4>${news.title}</h4>
+                <p>${news.details.slice(0, 200) + '...'}</p>
+                    <div class="row">
+                        <div class="col d-flex">
+                            <img class="rounded-circle w-25" src = "${news.author.img}">
+                            <div class ="pt-2">
+                                <p class="ps-3 mb-1">${news.author.name}</p>
+                                <p class="ps-3">${news.author.published_date}</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <h3 class ="pt-3 ms-5">${news.total_view}</h3>
+                        </div>
+                        <div class="col">
+                            <button onclick = "showDetails()" class="btn btn-primary mt-3 ms-5">Details</button>
+                        </div>
+                        </div>
+            </div>
+        `;
+        newsCategory.appendChild(newsDiv);
+    })
+};
+
+loadNews();
