3f76f5567f5624d76bfb4c11a684659ee60626ee
harunorrashid4419 <harunrashid4419@gmail.com>
2022-09-02 21:58:43 +0600

    category-add

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const loadCategory = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategory(data.data.news_category);
+}
+
+const displayCategory = categorys =>{
+    const categoryContainer = document.getElementById('category-section');
+    categorys.forEach(category =>{
+        console.log(category);
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('navbar-nav');
+        categoryDiv.innerHTML = `
+            <a class="nav-link active px-4" aria-current="page" href="#">${category.category_name}</a>
+        `;
+        categoryContainer.appendChild(categoryDiv);
+    })
+}
+
+loadCategory();
